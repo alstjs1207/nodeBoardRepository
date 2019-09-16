89edebc238c658719488b7cd59a7bb05c5f75bf5
alstjs1207 <alstjs1207@naver.com>
2019-09-16 19:55:37 +0900

    이미지 게시판 변경 기능 처리

--- nodeImageBoard/routes/board.js
+++ nodeImageBoard/routes/board.js
@@ -148,7 +148,7 @@ router.get('/view/:seq/:id', function(req, res, next) {
 	console.log("board list:  "+req.session.sid);
 	var seq = req.params.seq;
 	var id = req.params.id;
-	console.log("seq : "+seq +",id :"+id)
+	console.log("seq : "+seq +",id :"+id);
 	var query = "SELECT seq, boardcd, title, contents, userid, regdate, moddate FROM BOARD WHERE seq = ? AND userid = ?";
 	
 	var imageQuery="SELECT seq,	boardcd, filename,	filedir, vfiledir, regdate,	moddate FROM IMAGE WHERE boardcd = ?";
@@ -206,6 +206,113 @@ router.get('/modview/:seq/:id', function(req, res, next) {
 	  			
 	  		});
 			});
+		
+	}
+	else {
+		res.redirect('/main/login');
+	}
+});
+
+//수정 저장 화면
+router.post('/modview',upload.array('filenameModify'), function(req, res, next) {
+	console.log("board modview save!!");
+	console.log("board modview save :  "+req.session.sid);
+	
+	//var seq = req.params.seq;
+	//var id = req.params.id;
+	var body = req.body;
+	console.log("seq : " + body.seq);
+	if(req.session.sid) {
+		
+		//게시판 고유 코드 생성
+		var boardcd = "B" + moment().format("YYYYMMDDHHmmsss");
+		console.log("boardcd : " + boardcd);
+		
+		var query = "UPDATE BOARD SET contents = ?, boardcd = ?, moddate = SYSDATE() WHERE seq = ?";
+		var imageQuery="SELECT seq,	boardcd, filename,	filedir, vfiledir, regdate,	moddate FROM IMAGE WHERE boardcd = ?";
+		var imageUpdateQuery = "UPDATE IMAGE SET boardcd = ?, repyn = ? WHERE seq = ?";
+		var imageAddQuery = "INSERT INTO IMAGE (boardcd, filename, filedir,vfiledir, regdate, moddate, repyn) VALUE (?,?,?,?,SYSDATE(),SYSDATE(),?)";
+		
+		//이미지 업데이트
+		getConnection().query(query,[body.contents,boardcd,body.seq], function(err, boardView, fields){
+	  		if(err) {
+	  			console.log("error :"+err);
+	  			return;
+	  		}
+	  		console.log("board update!!");
+	  		var fileimage_rep_Yn = "";
+	  		var repImage = body.fileimage_rep; //선택된 대표 이미지
+	  		var filename = "";
+	  		//기존 저장된 이미지 리스트
+	  		getConnection().query(imageQuery,[body.boardcd], function(err, getImageViewList, fields){
+	  			if(err) {
+	  	  			console.log("error :"+err);
+	  	  			return;
+	  	  		}
+	  			console.log("image List!!");
+	  			//기존 저장된 이미지에서 대표이미지가 변경되었는지 확인 후 업데이트
+	  			for(var i = 0; i < getImageViewList.length; i++){
+	  				
+	  				filename = getImageViewList[i].filename;
+	  				var imageSeq = getImageViewList[i].seq;
+	  				console.log("image list : "+filename+","+imageSeq);
+	  				if(typeof repImage == "undefined" || repImage == null || repImage == ""){
+	  					repImage = filename;
+	  					fileimage_rep_Yn = "Y";
+	  				}
+	  				else if(filename === repImage) {
+	  					fileimage_rep_Yn = "Y";
+	  				} else {
+	  					fileimage_rep_Yn = "N";
+	  				}
+	  				// 대표이미지 변경 업데이트
+	  				getConnection().query(imageUpdateQuery,[boardcd,fileimage_rep_Yn,imageSeq], function(err, getImageViewList, fields){
+	  		  			if(err) {
+	  		  	  			console.log("error :"+err);
+	  		  	  			return;
+	  		  	  		}
+	  		  			console.log("image update!! "+boardcd +","+fileimage_rep_Yn+"," + imageSeq );
+	  				});
+	  				
+	  			}
+	  			
+	  			console.log("add image task");
+	  			//추가된 이미지 저장
+	  			var files = req.files;
+	  			var vfiledir = "";
+	  			
+	  			for(var j = 0; j < files.length; j++){
+	  				
+	  				filename = files[j].originalname;
+	  				vfiledir = "/image/" +filename;
+	  				
+	  				console.log(filename+","+vfiledir);
+	  				console.log("rep : " + repImage);
+	  				//대표 이미지 선정
+	  				//없을경우 첫 이미지로 대표 이미지 선택
+	  				if(typeof repImage == "undefined" || repImage == null || repImage == ""){
+	  					repImage = filename;
+	  					fileimage_rep_Yn = "Y";
+	  				}
+	  				else if(filename === repImage) {
+	  					fileimage_rep_Yn = "Y";
+	  				} else {
+	  					fileimage_rep_Yn = "N";
+	  				}
+	  			
+	  				getConnection().query(imageAddQuery,[boardcd,filename,vfiledir,vfiledir,fileimage_rep_Yn], function(err, rows, fields){
+	  				
+	  					if(err) {
+	  						console.log(err);
+	  					}
+	  					
+	  				});
+	  			}
+	  			
+	  			res.redirect('/board/list/1');
+	  			
+	  		});
+		});
 		
 	}
 	else {
